0d6cb56f674a7735b119576801d01ff9a80cdcc8
Erdie Flaviau Anjaratiana <erdie2anjaratiana1998@gmail.com>
2026-06-04 01:41:45 +0300

    REFRESH_URL

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const REFRESH_URL = import.meta.env.VITE_REFRESH_URL
+const REFRESH_URL = '/auth/token/refresh/'
 
 const axiosApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
